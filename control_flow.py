8de3935fcd176e260d274601f1f302acc7b426c0
newtonkiragu <karanunewton4@gmail.com>
2017-10-18 12:35:38 +0300

    elif statements

--- control_flow.py
+++ control_flow.py
@@ -2,6 +2,9 @@
 height = 64
 if height > 70:
     print("you are really tall")
+# we use elif when we have more than one condition to check for
+elif height > 60:
+    print("you are of average height")
 # else is used when the conditions passed evaluate to false
 else:
     print("you are really short")
