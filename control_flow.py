de3bf538b2c6cf428e164b1ac36287e23fb9db5c
newtonkiragu <karanunewton4@gmail.com>
2017-10-18 12:44:49 +0300

    range and list

--- control_flow.py
+++ control_flow.py
@@ -1,5 +1,6 @@
 # For loops
 # they are used when something is to be repeated a couple of times
-numbers = [1,2,3,4,5,6,7,8,9]
+# range is used when you want to go through a certain range of elements
+numbers = list(range(1,9))
 for number in numbers:
     print(number)
