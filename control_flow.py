b7b813e607d1be7fdadaac5f403dd79b0ec2346c
newtonkiragu <karanunewton4@gmail.com>
2017-10-18 12:33:17 +0300

    else statements

--- control_flow.py
+++ control_flow.py
@@ -1,4 +1,7 @@
 # if statements only run when the conditions passed to it evaluate to be true
-height = 74
+height = 64
 if height > 70:
     print("you are really tall")
+# else is used when the conditions passed evaluate to false
+else:
+    print("you are really short")
