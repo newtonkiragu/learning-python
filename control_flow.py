a9ae764965d57abd182b9a2df23f0983668dd1f8
newtonkiragu <karanunewton4@gmail.com>
2017-10-18 12:38:33 +0300

    empty values

--- control_flow.py
+++ control_flow.py
@@ -1,9 +1,10 @@
 # if statements only run when the conditions passed to it evaluate to be true
-height = 64
-if height > 70:
+# in python empty values are considered to be false
+height = []
+if height:
     print("you are really tall")
 # we use elif when we have more than one condition to check for
-elif height > 60:
+elif height:
     print("you are of average height")
 # else is used when the conditions passed evaluate to false
 else:
