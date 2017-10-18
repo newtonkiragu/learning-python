5b96c98a014b52e581afb320d2249c44969e0e5d
newtonkiragu <karanunewton4@gmail.com>
2017-10-18 12:43:24 +0300

    for loops

--- control_flow.py
+++ control_flow.py
@@ -1,11 +1,5 @@
-# if statements only run when the conditions passed to it evaluate to be true
-# in python empty values are considered to be false
-height = []
-if height:
-    print("you are really tall")
-# we use elif when we have more than one condition to check for
-elif height:
-    print("you are of average height")
-# else is used when the conditions passed evaluate to false
-else:
-    print("you are really short")
+# For loops
+# they are used when something is to be repeated a couple of times
+numbers = [1,2,3,4,5,6,7,8,9]
+for number in numbers:
+    print(number)
